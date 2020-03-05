ab79a75b3f16acfdcf62bd7d5f5ad63ddff3de42
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2020-03-05 13:18:46 +0100

    Zaimplementowano wybór ścieżki do plików - poprawki

--- src/app.py
+++ src/app.py
@@ -1,21 +1,25 @@
-import glob, os, shutil
+import os
 from tkinter import *
 from tkinter import messagebox, filedialog
 from PIL import ImageTk, Image
 
 folder_path = None
+
+
 def set_dir():
     # pobiera i zmienia sciezke lokalizacji plikow
     global folder_path
     filename = filedialog.askdirectory()
     folder_path.set(filename)
+    os.chdir(folder_path.get())
+    print(os.getcwd())
 
 
 def main():
     # ustawienia glownego okna
     main_window = Tk()
     main_window.title("HTML photogallery generator")
-    main_window.geometry("500x500+500+250")
+    main_window.geometry("500x550+500+250")
 
     #main_window.configure(background="gray")
     main_window.resizable(width=False, height=False)
@@ -23,6 +27,7 @@ def main():
     # ramki w aplikacji
 
     upper_frame = LabelFrame(main_window, text="Step 1", relief=GROOVE)
+    middle_frame = LabelFrame(main_window, text="Step 2", relief=GROOVE)
     lower_frame = LabelFrame(main_window, text="Nosacz", relief=GROOVE)
 
     # zmienne
@@ -33,10 +38,13 @@ def main():
     btn1 = Button(upper_frame, text="wybierz lokalizację", command=set_dir)
     btn1.pack(padx=15, side=LEFT)
 
+    btn2 = Button(middle_frame, text="say hi")
+    btn2.pack(padx=15, side=LEFT)
+
     # naklejki
 
-    lbl1 = Label(upper_frame, width=45, height=3, bg='SlateGray2', textvariable=folder_path)
-    lbl1.pack(side=TOP, pady=15, padx=10)
+    lbl1 = Label(upper_frame, width=45, height=2, bg='SlateGray2', textvariable=folder_path)
+    lbl1.pack(side=TOP, pady=7, padx=10)
 
     # super motywujacy obrazek
     my_img = ImageTk.PhotoImage(Image.open("images/nosacz.jpg"))
@@ -45,6 +53,7 @@ def main():
 
     # inicjalizacja ramek
     upper_frame.pack(padx=10, pady=10, fill=X)
+    middle_frame.pack(padx=10, fill=X)
     lower_frame.pack(padx=10, pady=10)
 
     main_window.mainloop()
