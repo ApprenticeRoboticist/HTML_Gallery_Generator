7a28630d39a046e02d3e729e3c9c22c9748eae57
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2020-03-05 14:47:23 +0100

    Implementacja numeracji zdjec w wybranym folderze

--- src/app.py
+++ src/app.py
@@ -15,13 +15,25 @@ def set_dir():
     print(os.getcwd())
 
 
+def rename():
+    i = 1
+    try:
+        for filename in os.listdir(os.getcwd()):
+            destined_name = str(i) + ".jpg"
+            source = filename
+
+            os.rename(source, destined_name)
+            i += 1
+    except:
+        messagebox.showinfo("Błąd", "Zdjęcia zostały już wcześniej ponumerowane!")
+
+
 def main():
-    # ustawienia glownego okna
     main_window = Tk()
+
+    # ustawienia glownego okna
     main_window.title("HTML photogallery generator")
     main_window.geometry("500x550+500+250")
-
-    #main_window.configure(background="gray")
     main_window.resizable(width=False, height=False)
 
     # ramki w aplikacji
@@ -30,21 +42,25 @@ def main():
     middle_frame = LabelFrame(main_window, text="Step 2", relief=GROOVE)
     lower_frame = LabelFrame(main_window, text="Nosacz", relief=GROOVE)
 
-    # zmienne
+    # zmienne globalne
     global folder_path
     folder_path = StringVar()
 
-    # przyciski
+    # Layout
+    # step1
     btn1 = Button(upper_frame, text="wybierz lokalizację", command=set_dir)
     btn1.pack(padx=15, side=LEFT)
 
-    btn2 = Button(middle_frame, text="say hi")
-    btn2.pack(padx=15, side=LEFT)
-
-    # naklejki
-
     lbl1 = Label(upper_frame, width=45, height=2, bg='SlateGray2', textvariable=folder_path)
     lbl1.pack(side=TOP, pady=7, padx=10)
+
+    # step 2
+
+    lbl2 = Label(middle_frame, width=30, height=2, text="Czy chcesz ponumerować zdjęcia?", font=("Tahoma", 10))
+    lbl2.pack(side=LEFT, pady=7, padx=15)
+
+    btn2 = Button(middle_frame, text="TAK", height=1, width=20, fg="black", command=rename, state=DISABLED)
+    btn2.pack(padx=30, side=LEFT)
 
     # super motywujacy obrazek
     my_img = ImageTk.PhotoImage(Image.open("images/nosacz.jpg"))
