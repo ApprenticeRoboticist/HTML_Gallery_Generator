da7df41e0b09ac614a53db9a0a447f3a3d1f3312
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2020-03-10 15:08:29 +0100

    Rozwiazano problem wcisniecia przycisku do zmiany nazwy plikow przed wybraniem odpowiedniego folderu

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 from tkinter import *
 from tkinter import messagebox, filedialog
+from tkinter.ttk import Combobox
 from PIL import ImageTk, Image
 
 folder_path = None
@@ -40,7 +41,9 @@ def main():
 
     upper_frame = LabelFrame(main_window, text="Step 1", relief=GROOVE)
     middle_frame = LabelFrame(main_window, text="Step 2", relief=GROOVE)
-    lower_frame = LabelFrame(main_window, text="Nosacz", relief=GROOVE)
+    lower_frame = LabelFrame(main_window, text="Step3", relief=GROOVE)
+    mini_frame = LabelFrame(lower_frame, relief=FLAT)
+    mini_frame2 = LabelFrame(lower_frame, relief=FLAT)
 
     # zmienne globalne
     global folder_path
@@ -48,7 +51,7 @@ def main():
 
     # Layout
     # step1
-    btn1 = Button(upper_frame, text="wybierz lokalizację", command=lambda: [set_dir(), btn2.config(state=NORMAL)])
+    btn1 = Button(upper_frame, text="wybierz lokalizację", command=lambda: [set_dir(), btn2.config(state=NORMAL)], relief=GROOVE)
     btn1.pack(padx=15, side=LEFT)
 
     lbl1 = Label(upper_frame, width=45, height=2, bg='SlateGray2', textvariable=folder_path)
@@ -59,19 +62,43 @@ def main():
     lbl2 = Label(middle_frame, width=30, height=2, text="Czy chcesz ponumerować zdjęcia?", font=("Tahoma", 10))
     lbl2.pack(side=LEFT, pady=7, padx=15)
 
-    btn2 = Button(middle_frame, text="TAK", height=1, width=20, fg="black", command=rename, state=DISABLED)
+    btn2 = Button(middle_frame, text="TAK", height=1, width=20, fg="black", command=rename, state=DISABLED, relief=GROOVE)
     btn2.pack(padx=30, side=LEFT)
-    #btn2.config(state=NORMAL)
+
+    # step 3
+
+    mini_frame.pack(side=TOP)
+
+    lbl3 = Label(mini_frame, width=40, height=1, text="Wybierz ilość zdjeć w danym kroku instrukcji:", font=("Tahoma", 10))
+    lbl3.pack(side=LEFT, padx=10, pady=10)
+
+    combo = Combobox(mini_frame)
+    combo['values'] = (1, 2, 3, 4, 5, 6, 7, 8)
+    combo.current(0)
+    combo.pack(side=RIGHT, padx=25, pady=15)
+
+    mini_frame2.pack(side=TOP)
+
+    btn3 = Button(mini_frame2, text="Dodaj krok", width=15, relief=GROOVE)
+    btn3.pack(side=LEFT, padx=22, pady=5)
+    btn4 = Button(mini_frame2, text="Usuń poprzedni", width=15, relief=GROOVE)
+    btn4.pack(side=LEFT, padx=22, pady=5)
+    btn5 = Button(mini_frame2, text="Usuń wszystkie", width=15, relief=GROOVE)
+    btn5.pack(side=LEFT, padx=22, pady=5)
+
+    txt = Text(lower_frame, width=40, height=7)
+    txt.pack(side=TOP, padx=15, pady=10)
+    txt.insert(INSERT, "Co jest kurwa?")
 
     # super motywujacy obrazek
-    my_img = ImageTk.PhotoImage(Image.open("images/nosacz.jpg"))
-    new_image = Label(lower_frame, image=my_img)
-    new_image.pack(padx=5)
+    #my_img = ImageTk.PhotoImage(Image.open("images/nosacz.jpg"))
+    #new_image = Label(right_frame, image=my_img)
+    #new_image.pack(padx=5)
 
     # inicjalizacja ramek
     upper_frame.pack(padx=10, pady=10, fill=X)
     middle_frame.pack(padx=10, fill=X)
-    lower_frame.pack(padx=10, pady=10)
+    lower_frame.pack(padx=10, pady=10, fill=X)
 
     main_window.mainloop()
 
