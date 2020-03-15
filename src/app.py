4fd263700e50bf2be1b2ab70bb67df4960b0a186
Moony <51023622+ApprenticeRoboticist@users.noreply.github.com>
2020-03-15 21:51:27 +0100

    Dodano krok w który pobiera nazwe instrukcji od uzytkownika i zapisuje ja w zmiennej globalnej "title"

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from tkinter.ttk import Combobox
 from PIL import ImageTk, Image
 
 folder_path = None
+title = None
 
 
 def set_dir():
@@ -32,6 +33,13 @@ def rename():
         messagebox.showinfo("Błąd", "Zdjęcia zostały już wcześniej ponumerowane!")
 
 
+def get_name(value):
+    """Zbiera dane z okna wejscia w kroku 3 i przypisuje je do zmiennej globalnej title"""
+    global title
+    title = value
+    print(title)
+
+
 def add_value(chapter_list, value):
     chapter_list.append(value)
     print(chapter_list)
@@ -47,24 +55,36 @@ def main():
 
     # ustawienia glownego okna
     main_window.title("HTML photogallery generator")
-    main_window.geometry("500x550+500+250")
+    main_window.geometry("500x650+500+125")
     main_window.resizable(width=False, height=False)
 
     # ramki w aplikacji
 
     upper_frame = LabelFrame(main_window, text="Step 1", relief=GROOVE)
-    middle_frame = LabelFrame(main_window, text="Step 2", relief=GROOVE)
-    lower_frame = LabelFrame(main_window, text="Step3", relief=GROOVE)
-    mini_frame = LabelFrame(lower_frame, relief=FLAT)
-    mini_frame2 = LabelFrame(lower_frame, relief=FLAT)
+
+    middle_frame1 = LabelFrame(main_window, text="Step 2", relief=GROOVE)
+
+    middle_frame2 = LabelFrame(main_window, text="Step 3", relief=GROOVE)
+    middle2_mini_frame = LabelFrame(middle_frame2, relief=FLAT)
+
+    lower_frame = LabelFrame(main_window, text="Step 4", relief=GROOVE)
+    lower_mini_frame1 = LabelFrame(lower_frame, relief=FLAT)
+    lower_mini_frame2 = LabelFrame(lower_frame, relief=FLAT)
 
     # zmienne globalne
     global folder_path
     folder_path = StringVar()
 
+    # zmienne
+    title = ""
+    value_list = []
+
     # Layout
     # step1
-    btn1 = Button(upper_frame, text="wybierz lokalizację", command=lambda: [set_dir(), btn2.config(state=NORMAL)], relief=GROOVE)
+    btn1 = Button(upper_frame, text="wybierz lokalizację",
+                  command=lambda: [set_dir(), btn2.config(state=NORMAL), btn3.config(state=NORMAL),
+                                   en1.config(state=NORMAL)],
+                  relief=GROOVE)
     btn1.pack(padx=15, side=LEFT)
 
     lbl1 = Label(upper_frame, width=45, height=2, bg='SlateGray2', textvariable=folder_path)
@@ -72,37 +92,49 @@ def main():
 
     # step 2
 
-    lbl2 = Label(middle_frame, width=30, height=2, text="Czy chcesz ponumerować zdjęcia?", font=("Tahoma", 10))
+    lbl2 = Label(middle_frame1, width=30, height=2, text="Czy chcesz ponumerować zdjęcia?", font=("Tahoma", 10))
     lbl2.pack(side=LEFT, pady=7, padx=15)
 
-    btn2 = Button(middle_frame, text="TAK", height=1, width=20, fg="black", command=rename, state=DISABLED, relief=GROOVE)
+    btn2 = Button(middle_frame1, text="TAK", height=1, width=20, fg="black", command=rename, state=DISABLED,
+                  relief=GROOVE)
     btn2.pack(padx=30, side=LEFT)
 
     # step 3
 
-    mini_frame.pack(side=TOP)
+    middle2_mini_frame.pack(side=TOP, fill=X)
+    lbl3 = Label(middle2_mini_frame, width=25, height=1, text="Podaj nazwę instrukcji", font=("Tahoma", 10))
+    lbl3.pack(side=LEFT)
+    en1 = Entry(middle2_mini_frame, width=35, textvariable=title, state=DISABLED)
+    en1.pack(side=RIGHT, padx=25, pady=5)
+    btn3 = Button(middle_frame2, text="Zatwierdź nazwę", height=1, width=20, fg="black",
+                  command=lambda: [btn3.config(state=DISABLED), get_name(en1.get()), en1.config(state=DISABLED)],
+                  state=DISABLED, relief=GROOVE)
+    btn3.pack(side=BOTTOM, padx=30, pady=5)
 
-    lbl3 = Label(mini_frame, width=40, height=1, text="Wybierz ilość zdjeć w danym kroku instrukcji:", font=("Tahoma", 10))
-    lbl3.pack(side=LEFT, padx=10, pady=10)
+    # step 4
 
-    combo = Combobox(mini_frame)
+    lower_mini_frame1.pack(side=TOP)
+
+    lbl4 = Label(lower_mini_frame1, width=40, height=1, text="Wybierz ilość zdjeć w danym kroku instrukcji:", font=("Tahoma", 10))
+    lbl4.pack(side=LEFT, padx=10, pady=5)
+
+    combo = Combobox(lower_mini_frame1)
     combo['values'] = (1, 2, 3, 4, 5, 6, 7, 8)
     combo.current(0)
     combo.pack(side=RIGHT, padx=25, pady=15)
-    value_list = []
 
-    mini_frame2.pack(side=TOP)
+    lower_mini_frame2.pack(side=TOP)
 
-    btn3 = Button(mini_frame2, text="Dodaj krok", width=15, relief=GROOVE, command=lambda: add_value(value_list, combo.get()))
-    btn3.pack(side=LEFT, padx=22, pady=5)
-    btn4 = Button(mini_frame2, text="Usuń poprzedni", width=15, relief=GROOVE, command=lambda: delete_value(value_list))
+    btn4 = Button(lower_mini_frame2, text="Dodaj krok", width=15, relief=GROOVE, command=lambda: add_value(value_list, combo.get()))
     btn4.pack(side=LEFT, padx=22, pady=5)
-    btn5 = Button(mini_frame2, text="Usuń wszystkie", width=15, relief=GROOVE)
+    btn5 = Button(lower_mini_frame2, text="Usuń poprzedni", width=15, relief=GROOVE, command=lambda: delete_value(value_list))
     btn5.pack(side=LEFT, padx=22, pady=5)
+    btn6 = Button(lower_mini_frame2, text="Usuń wszystkie", width=15, relief=GROOVE)
+    btn6.pack(side=LEFT, padx=22, pady=5)
 
-    txt = scrolledtext.ScrolledText(lower_frame, width=40, height=12, state=DISABLED)
+    txt = scrolledtext.ScrolledText(lower_frame, width=40, height=12)
     txt.pack(side=TOP, padx=15, pady=10)
-    txt.insert(INSERT, "Co jest kurwa?")
+    txt.insert(INSERT, "Tekst inicjalizacyjny")
 
     # super motywujacy obrazek
     #my_img = ImageTk.PhotoImage(Image.open("images/nosacz.jpg"))
@@ -110,9 +142,10 @@ def main():
     #new_image.pack(padx=5)
 
     # inicjalizacja ramek
-    upper_frame.pack(padx=10, pady=10, fill=X)
-    middle_frame.pack(padx=10, fill=X)
-    lower_frame.pack(padx=10, pady=10, fill=X)
+    upper_frame.pack(side=TOP, padx=10, pady=10, fill=X)
+    middle_frame1.pack(side=TOP, padx=10, fill=X)
+    middle_frame2.pack(side=TOP, padx=10, fill=X)
+    lower_frame.pack(side=TOP, padx=10, pady=10, fill=X)
 
     main_window.mainloop()
 
